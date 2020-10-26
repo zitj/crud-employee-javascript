2084b334333717dd448d04d5a9f4bb8f671ee873
zitj <1eyedea2eyedeas@gmail.com>
2020-10-26 10:56:54 +0100

    added local storage

--- script/script.js
+++ script/script.js
@@ -21,31 +21,54 @@ class Employee{
         this.email = email;
         this.mobile = mobile;
     }
-
+    
     showData()
     {
-   
+        Employee.renderHtml(this.id,this.name,this.email,this.mobile);
+        return this;
+    }
+    
+    storeEmployee()
+    {
+        const allData = JSON.parse(localStorage.getItem('employees')) ?? [];
+        allData.push({id:this.id,name:this.name,email:this.email,mobile:this.mobile});
+        localStorage.setItem('employees',JSON.stringify(allData));
+    }
+
+    static showAllEmployees()
+    {
+        if(localStorage.getItem('employees')){
+            JSON.parse(localStorage.getItem('employees')).forEach((item)=>{
+                Employee.renderHtml(item.id,item.name,item.email,item.mobile)
+        })
+    }
+    }
+    static renderHtml(id,name,email,mobile)
+    {
         const trEl = document.createElement('tr');
-        trEl.innerHTML = `
-            <tr>
-            <td>${this.name}</td>
-            <td>${this.email}</td>
-            <td>${this.mobile}</td>
-            <td>
-                <button>Edit</button>
-                <button>Delete</button>
-            </td>
-            </tr>
-        `
-        table.appendChild(trEl);
+                trEl.innerHTML = `
+                    <tr>
+                    <td>${name}</td>
+                    <td>${email}</td>
+                    <td>${mobile}</td>
+                    <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </td>
+                    </tr>
+                `
+                table.appendChild(trEl);
+            
     }
 }
+
+Employee.showAllEmployees();
 
 storeDataButton.addEventListener('click', (e)=>{
     e.preventDefault();
     let id = Math.floor(Math.random() * 1000000);
     const newEmp = new Employee(id,nameInput.value, emailInput.value, mobileInput.value);
-    newEmp.showData();
+    newEmp.showData().storeEmployee();
     nameInput.value = '';
     emailInput.value = '';
     mobileInput.value = '';
