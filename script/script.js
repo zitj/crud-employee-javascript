615bfd5097684bf631ee01d5a02df5ad22d5d1ac
zitj <1eyedea2eyedeas@gmail.com>
2020-10-26 13:46:36 +0100

    app finished

--- script/script.js
+++ script/script.js
@@ -4,14 +4,14 @@ const formContainer = document.querySelector('.form-container');
 const dataContainer = document.querySelector('.data-container');
 const inputs = formContainer.querySelectorAll('input');
 
-const storeDataButton = formContainer.querySelector('button');
+const storeDataButton = document.getElementById('submit');
 const table = dataContainer.querySelector('table');
 
 const nameInput = inputs[0];
 const emailInput = inputs[1];
 const mobileInput = inputs[2];
 
-
+const controlIdEdit = document.getElementById('controlIdEdit');
 class Employee{
     constructor(id,name,email,mobile)
     {
@@ -42,6 +42,20 @@ class Employee{
         })
     }
     }
+    //update element
+    updateEmployee(id)
+    {
+        const newItem = {id:id,name:this.name,email:this.email,mobile:this.mobile};
+        const UpdatedData = JSON.parse(localStorage.getItem('employees')).map((item)=>{
+            if(item.id == id){
+                return newItem;
+            }
+            return item;
+        })
+
+        localStorage.setItem('employees', JSON.stringify(UpdatedData));
+    }
+
     static renderHtml(id,name,email,mobile)
     {
         const trEl = document.createElement('tr');
@@ -51,7 +65,7 @@ class Employee{
                     <td>${email}</td>
                     <td>${mobile}</td>
                     <td>
-                        <button class="edit">Edit</button>
+                        <button class="edit" data-id="${id}">Edit</button>
                         <button class="delete" data-id="${id}">Delete</button>
                     </td>
                     </tr>
@@ -64,10 +78,26 @@ class Employee{
 Employee.showAllEmployees();
 
 storeDataButton.addEventListener('click', (e)=>{
+    
     e.preventDefault();
-    let id = Math.floor(Math.random() * 1000000);
-    const newEmp = new Employee(id,nameInput.value, emailInput.value, mobileInput.value);
-    newEmp.showData().storeEmployee();
+    
+    if(nameInput.value == '' || emailInput.value == '' || mobileInput.value == ''){
+        return;
+    }
+    if(!controlIdEdit.value){
+        let id = Math.floor(Math.random() * 1000000);
+        const newEmp = new Employee(id,nameInput.value, emailInput.value, mobileInput.value);
+        newEmp.showData().storeEmployee();
+    }
+    else
+    {
+        const id = controlIdEdit.value;
+        const newEmp = new Employee(id,nameInput.value, emailInput.value, mobileInput.value);
+        newEmp.updateEmployee(id);
+        storeDataButton.value = 'Update data';
+        table.innerHTML = '';
+        Employee.showAllEmployees();
+    }
     nameInput.value = '';
     emailInput.value = '';
     mobileInput.value = '';
@@ -80,11 +110,24 @@ dataContainer.addEventListener('click', (e)=>{
         //remove from locals
         const id = +e.target.getAttribute("data-id");
         const emps = JSON.parse(localStorage.getItem('employees'));
-        const newData = emps.filter(item => item.id !== id);
+        const newData = emps.filter(item => item.id != id);
         localStorage.setItem('employees', JSON.stringify(newData));
         
         // remove from html
         e.target.parentElement.parentElement.remove();
 
+    }
+  
+    if(e.target.classList.contains('edit'))
+    {
+
+        const id = +e.target.getAttribute("data-id");
+        const item = JSON.parse(localStorage.getItem('employees')).find(item => item.id == id);
+        nameInput.value = item.name;
+        emailInput.value = item.email;
+        mobileInput.value = item.mobile;
+        controlIdEdit.value = id;
+        storeDataButton.value = 'Update data';
+        console.log(item);
     }
 });
